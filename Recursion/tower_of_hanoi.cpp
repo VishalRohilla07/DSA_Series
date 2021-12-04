2daf0c38ef976f5b9660c9de0cad52444a12c54c
Vishal <40372944+VishalRohilla07@users.noreply.github.com>
2021-12-04 15:04:24 +0000

    Tower of Hanoi Problem

---
+++ Recursion/tower_of_hanoi.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void towerofhanoi(int n ,char src, char dest, char helper){
+
+  if(n==0){
+    return; //base case
+  }
+
+  towerofhanoi(n-1,src,helper,dest);
+  cout<<"Move from"<<src<<"to"<<dest<<endl;
+  towerofhanoi(n-1,helper,dest,src);
+}
+
+int main(){
+  towerofhanoi(3,'A','B','C');
+  return 0;
+}
