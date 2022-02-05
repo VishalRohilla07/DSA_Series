a22c55c26f3eb711389331f6533e56fb674ead08
Vishal <40372944+VishalRohilla07@users.noreply.github.com>
2022-02-05 15:00:09 +0000

    check balanced

---
+++ Binary_Tree/check_balanced.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+
+template <typename T>
+class BinaryTreeNode {
+   public:
+    T data;
+    BinaryTreeNode<T>* left;
+    BinaryTreeNode<T>* right;
+
+    BinaryTreeNode(T data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+using namespace std;
+
+#include <cmath>
+
+int height(BinaryTreeNode<int> *root)
+{   if(root==NULL)
+       return 0;
+ 
+    return 1+max(height(root->left),height(root->right));
+}
+bool isBalanced(BinaryTreeNode<int> *root) {
+    
+    if (root == NULL) 
+        return 1; 
+    int l=height(root->left);
+	int r=height(root->right);	
+   
+     if ( ( abs(l- r) ==0 || abs(l-r) == 1 ) && isBalanced(root->left) && isBalanced(root->right))
+        return 1;
+    
+   return 0;
+}
+
+BinaryTreeNode<int>* takeInput() {
+    int rootData;
+    cin >> rootData;
+    if (rootData == -1) {
+        return NULL;
+    }
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    queue<BinaryTreeNode<int>*> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode<int>* currentNode = q.front();
+        q.pop();
+        int leftChild, rightChild;
+
+        cin >> leftChild;
+        if (leftChild != -1) {
+            BinaryTreeNode<int>* leftNode = new BinaryTreeNode<int>(leftChild);
+            currentNode->left = leftNode;
+            q.push(leftNode);
+        }
+
+        cin >> rightChild;
+        if (rightChild != -1) {
+            BinaryTreeNode<int>* rightNode =
+                new BinaryTreeNode<int>(rightChild);
+            currentNode->right = rightNode;
+            q.push(rightNode);
+        }
+    }
+    return root;
+}
+
+int main() {
+    BinaryTreeNode<int>* root = takeInput();
+    cout << (isBalanced(root) ? "true" : "false");
+}
